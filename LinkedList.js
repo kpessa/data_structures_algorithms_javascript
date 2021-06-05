fb603cb9f7526f4a6a4c42f5a42f12a5950e174d
Kurt Pessa <kpessa@gmail.com>
2021-06-05 09:20:08 -0400

    initial commit

---
+++ LinkedList.js
@@ -0,0 +1,82 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = null;
+  }
+  toString() {
+    return `${this.value} -> ${this.next}`;
+  }
+}
+
+class LinkedList {
+  // create a new node
+  constructor(value) {
+    this.head = this.tail = new Node(value);
+    this.length = 1;
+  }
+  to_a() {
+    if (!this.head) return [];
+    const a = [];
+    let temp = this.head;
+    a.push(temp.value);
+    while (temp.next) {
+      temp = temp.next;
+      a.push(temp.value);
+    }
+    return a;
+  }
+  toString() {
+    return `Linked list: ${this.to_a()}
+    head: ${this.head}
+    tail: ${this.tail}`;
+  }
+  push(value) {
+    const newNode = new Node(value); // create a new node
+    if (!this.head) this.head = this.tail = newNode;
+    else this.tail.next = this.tail = newNode; // add Node to end
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined; // 0 items edgecase
+    let pre = this.head;
+    let temp = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) this.head = this.tail = null; // 1 item edgecase
+    return temp;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    const temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return temp;
+  }
+  unshift(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    if (this.length === 1) this.tail = this.head;
+    return true;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = index; i--; ) temp = temp.next;
+    return temp;
+  }
+  // create new Node
+  // insert Node
+  insert(index, value) {}
+}
+
+module.exports = LinkedList;
---
+++ test.js
@@ -0,0 +1,7 @@
+const LinkedList = require('./LinkedList');
+
+const ll = new LinkedList(1);
+ll.push(2);
+ll.push(3);
+ll.push(4);
+console.log(ll.get(0));
