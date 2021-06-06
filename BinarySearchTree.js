874431d80f7cc7d66f9775033fa0fd665ed76d9b
Kurt Pessa <kpessa@gmail.com>
2021-06-06 17:29:44 -0400

    Worked on binary search tree

---
+++ BinarySearchTree.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+}
+
+let bst = new BST();
+bst.insert(47);
+bst.insert(21);
+bst.insert(18);
+bst.insert(76);
+bst.insert(52);
+bst.insert(82);
+bst.insert(27);
+bst;
